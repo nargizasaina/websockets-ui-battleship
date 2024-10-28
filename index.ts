45652b005a8c6caea933ef0ac98d5ec9b0f9c0f8
Nargiza Sainazarova <nargizasaina@gmail.com>
2024-10-28 19:52:40 +0600

    feat: add connection

--- index.ts
+++ index.ts
@@ -1,6 +1,24 @@
+import { WebSocketServer } from 'ws';
 import { httpServer } from "./src/http_server/index";
 
 const HTTP_PORT = 8181;
+
+const wss = new WebSocketServer({server: httpServer}); 
+console.log(`WebSocket server started on ws://localhost:${HTTP_PORT}`);
+
+wss.on('connection', (ws) => {
+    console.log('New WebSocket connection established');
+
+    ws.on('message', (message) => {
+      console.log(`Received command: ${message}`);
+    });
+
+    ws.on('close', (close) => {
+      console.log('WebSocket connection closed');
+    });
+
+    ws.send('Welcome to the WebSocket server!');
+});
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
